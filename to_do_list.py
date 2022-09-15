5da310ea692b066d0f0f2cc0d0eb9368729286a4
0xbm <0xmb@protonmail.com>
2022-09-15 19:18:02 +0200

    add 2 functions

--- to_do_list.py
+++ to_do_list.py
@@ -1,1 +1,14 @@
-list = []
+reminder_list = []
+
+def addReminder():
+  while (command := input("Add reminder >>> ")) != "exit":
+    print("Your command was:", command)
+    reminder_list.append(command)
+  print(reminder_list)
+
+
+def delReminder():
+    print(reminder_list)
+
+addReminder()
+delReminder()
